e4f0f3a3f960808724f46450d39523fdc81116a3
Виатлий <vitaliy.guselnikov@ucann.ru>
2020-02-27 14:57:42 +0300

    Observer pattern with Proxy

---
+++ src/Services/observer.js
@@ -0,0 +1,20 @@
+function Observer (target) {
+
+  return new Proxy(target, {
+    set (target, property, value, receiver) {
+      const success = Reflect.set(target, property, value, receiver)
+      if (success) {
+        console.log('QQQ', property, target[property])
+      }
+      return success
+    }
+  })
+}
+
+const obj = {}
+const observer = Observer(obj)
+observer.name = 'wqe'
+observer.name1 = 'wqe1'
+observer.name2 = 'wqe2'
+observer.name3 = 'wqe3'
+observer.name = 'wqe123'
